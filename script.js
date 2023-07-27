0e82a0a4b04712abfbc774c72b8c6a60a7ada1b3
scstaging <scstaging@hotmail.com>
2023-07-27 09:40:50 -0400

    Changed copy, variable font sizes

--- script.js
+++ script.js
@@ -17,29 +17,29 @@ let width = fontContainer.offsetWidth;
 function resizeText(width, variableFontSize) {
     width = fontContainer.offsetWidth;
     if (width < 200) {
-        variableFontSize.style.fontSize = '6px';
+        variableFontSize.style.fontSize = '8px';
         variableFontSize.style.lineHeight = '12px'
     } else if (width > 200 && width < 300) {
-        variableFontSize.style.fontSize = '7px';
+        variableFontSize.style.fontSize = '9px';
         variableFontSize.style.lineHeight = '20px'
     } else if (width > 300 && width < 400) {
-        variableFontSize.style.fontSize = '10px';
-        variableFontSize.style.lineHeight = '35px'
+        variableFontSize.style.fontSize = '14px';
+        variableFontSize.style.lineHeight = '30px'
     } else if (width > 400 && width < 500) {
-        variableFontSize.style.fontSize = '12px';
-        variableFontSize.style.lineHeight = '45px'
-    } else if (width > 500 && width < 650) {
         variableFontSize.style.fontSize = '18px';
+        variableFontSize.style.lineHeight = '40px'
+    } else if (width > 500 && width < 650) {
+        variableFontSize.style.fontSize = '22px';
         variableFontSize.style.lineHeight = '50px'
     } else if (width > 650 && width < 800) {
-        variableFontSize.style.fontSize = '22px';
+        variableFontSize.style.fontSize = '28px';
         variableFontSize.style.lineHeight = '70px'
     } else if (width > 800 && width < 1000) {
-        variableFontSize.style.fontSize = '26px';
-        variableFontSize.style.lineHeight = '70px'
-    } else if (width > 1000 && width < 1200) {
         variableFontSize.style.fontSize = '30px';
-        variableFontSize.style.lineHeight = '80px'
+        variableFontSize.style.lineHeight = '90px'
+    } else if (width > 1000 && width < 1200) {
+        variableFontSize.style.fontSize = '40px';
+        variableFontSize.style.lineHeight = '100px'
     }
 }
 
